f551e67bb904c89a5395c51ad2aac10867c1f160
Plavskikh Peter <qksyo@yandex.ru>
2022-05-07 18:51:01 +0700

    vernul

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -4,13 +4,13 @@ const { login, createUser } = require('../controllers/userController');
 
 const registerRoutes = express.Router();
 
-registerRoutes.post('/signin', celebrate({
+registerRoutes.post('/signin', express.json(), celebrate({
   body: Joi.object().keys({
     email: Joi.string().email().required().max(30),
     password: Joi.string().required(),
   }),
 }), login);
-registerRoutes.post('/signup', celebrate({
+registerRoutes.post('/signup', express.json(), celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().min(2).max(30),
     password: Joi.string().required(),
