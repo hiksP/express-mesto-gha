953646a43cb2d0b6abad752a6faa43b292ff39ee
Plavskikh Peter <qksyo@yandex.ru>
2022-05-07 18:47:12 +0700

    check

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -4,13 +4,13 @@ const { login, createUser } = require('../controllers/userController');
 
 const registerRoutes = express.Router();
 
-registerRoutes.post('/signin', express.json(), celebrate({
+registerRoutes.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().email().required().max(30),
     password: Joi.string().required(),
   }),
 }), login);
-registerRoutes.post('/signup', express.json(), celebrate({
+registerRoutes.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().min(2).max(30),
     password: Joi.string().required(),
